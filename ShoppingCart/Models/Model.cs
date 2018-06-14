4244d8a18df238505c642b7d5b8188cd04f7df62
Tomasz Weclawski <tomasz.weclawski@ms.polsl.local>
2018-06-14 15:21:07 +0200

    Added some MVP logic.

--- ShoppingCart/Models/Model.cs
+++ ShoppingCart/Models/Model.cs
@@ -10,8 +10,10 @@ namespace ShoppingCart.Models
     public class Model
     {
         //Product product;
-        public List<Product> Products = new List<Product>();
-        public List<Product> Cart = new List<Product>();
+        public List<Product> Products { get; set; }
+        public List<Product> Cart { get; set; }
+
+        
         public double Cost { get; set; }
 
         public void AddProductToProducts(Product product)
